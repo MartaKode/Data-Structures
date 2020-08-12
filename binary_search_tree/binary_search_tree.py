f98160b83a1177d14efc6f48a34920eab44da332
Marta Krawczyk <martaalicja113@gmail.com>
2020-08-12 16:13:42 -0400

    pushing changes for Binary Search Tree, tests passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,80 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # check if the value is less than the current node's value
+        if value < self.value:
+            # does the current node have a left child?
+            if self.left:
+                self.left.insert(value)
+            # otherwise, it doesn't have a left child
+            # we can park the new node here
+            else:
+                self.left = BSTNode(value)
+        # otherwise the value is greater or equal  to the current node's value
+        else:
+            # does the current node have a right child?
+            if self.right:
+                # if it does, call the right child's `insert` method to repeat the process
+                self.right.insert(value)
+            # otherwise, it doesn't have a right child
+            # we can park the new node here
+            else:
+                self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
-    def contains(self, target):
-        pass
+    def contains(self, target): # can implement using recursion
+        # pass
+        # check if the target value is less than the current node's value 
+        # if it is, we also want to know if the left child exists
+        if target < self.value and self.left is not None:
+            # use recursion to restart the cycle and check if the left child contains the value 
+            return self.left.contains(target)
+        # check if value is equal to the current node's value
+        elif target == self.value:
+            return True
+        # otherwise value is greater than current node
+        # so we want to check if there is a right child
+        elif self.right is not None:
+            # restart the process with recursion to check if the right child contains the target
+            return self.right.contains(target)
+        # otherwise, BST does NOT contain target
+        else:
+            return False
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # pass
+        # keep track of max value
+        max_val = self.value
+        
+        # while there is a right child
+        while self.right is not None:
+            # set max value to right childs value
+            max_val = self.right.value
+            # set self to its right child
+            self = self.right  
+        # return max_val    
+        return max_val
 
     # Call the function `fn` on the value of each node
-    def for_each(self, fn):
-        pass
+    # This method doesn't return anything
+    def for_each(self, fn): # 
+        # pass
+        # call fn on the root nodes value
+        fn(self.value)
+        # check if there is a right child
+        if self.right:
+            # restart the process on the right child with recursion 
+            self.right.for_each(fn)
+        # otherwise there is no right child - we don't care
+
+        # check if there is a left child 
+        if self.left:
+            # restart the process with recursion on the ledfft child
+            self.left.for_each(fn)
+
 
     # Part 2 -----------------------
 
@@ -63,7 +123,7 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+bst = BSTNode(1)
 
 bst.insert(8)
 bst.insert(5)
@@ -80,6 +140,78 @@ print("elegant methods")
 print("pre order")
 bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
+# bst.in_order_dft()
 print("post order")
 bst.post_order_dft()  
+
+# # ```` my checks `````
+# print(bst.contains(8))
+# print(bst.get_max())
+
+
+# 🍝 Spaghetti code
+
+
+    # def contains(self, target): # can implement using recursion
+    #     # pass
+    #     # keep track of whether we're returning true or false
+    #     contains = False
+    #     # check if the target value is less than the current node's value 
+    #     # if it is, we also want to know if the left child exists
+    #     # so we can check for both!
+    #     if target < self.value and self.left is not None:
+    #         # # does the current node have a left child? 
+    #         # if self.left:
+    #             # # check if the left childs value is equal to target
+    #             # if self.left.value == target:
+    #             #     # contains is True
+    #             #     contains = True
+
+    #             # else:
+    #         # use recursion to restart the cycle and check if the left child contains the value
+    #         return self.left.contains(target)
+    #         # # otherwise, doesn't have a left child
+    #         # else:
+    #         #     # contains is False
+    #         #     contains = False
+
+    #     # check if value is equal to the current node's value
+    #     elif target == self.value:
+    #         # contains is True
+    #         contains = True
+
+    #     # otherwise value is greater than or equal to current node's value
+    #     elif self.right is not None:
+    #         # # check if current node has a right child
+    #         # if self.right:
+    #         #     # check if the right value is equal to target
+    #         #     if self.right.value == target:
+    #         #         # contains is True
+    #         #         contains = True
+
+    #         #     else:
+    #         #         # use recursion to restart the process
+    #         return self.right.contains(target)
+    #         # # otherwise, current node doesn't have a right child
+    #         # else:
+    #         #     # contains is False
+    #         #     contains = False
+
+    #     return contains
+
+
+
+    # def contains(self, target):
+    #     # check to see if the target is the the current node's value
+    #     if self.value == target:
+    #         return True
+    #     # check if there is a left leaf and the target is less than the current node's value
+    #     if self.left and target < self.value:
+    #         # check if the left leaf contains the value
+    #         return self.left.contains(target)
+    #     # chek if there is a right leaf
+    #     if self.right:
+    #         # check if the right leaf contains the value
+    #         return self.right.contains(target)
+    #     # if the target is not the current node's value or the right or left leaf (if they exist), return False
+    #     return False
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,12 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev  
             
 """
 Our doubly-linked list class. It holds references to 
@@ -37,7 +43,7 @@ class DoublyLinkedList:
             self.tail = new_node
             # update length
             self.length +=1
-            # return self.head
+
         # otherwise DLL is not empty
         else:
             # connect the nodes themselves: 
@@ -50,7 +56,7 @@ class DoublyLinkedList:
             old_head.prev = new_node 
             # update length
             self.length += 1
-            # return self.head
+
         
     """
     Removes the List's current head node, making the
@@ -256,17 +262,60 @@ class DoublyLinkedList:
             return max_val
 
 
-dll = DoublyLinkedList()
-dll.add_to_tail(1)
-# print(dll.get_max())
-dll.add_to_tail(100)
-# print(dll.get_max())
-dll.add_to_tail(99)
-# print(dll.get_max())
-dll.add_to_tail(101)
-# print(dll.get_max())
-print(dll.head.value)
-print(dll.tail.value)
-dll.move_to_end(dll.head)
-print(dll.head.value)
-print(dll.tail.value)
+# dll = DoublyLinkedList()
+# dll.add_to_tail(1)
+# # print(dll.get_max())
+# dll.add_to_tail(100)
+# # print(dll.get_max())
+# dll.add_to_tail(99)
+# # print(dll.get_max())
+# dll.add_to_tail(101)
+# # print(dll.get_max())
+# print(dll.head.value)
+# print(dll.tail.value)
+# dll.move_to_end(dll.head)
+# print(dll.head.value)
+# print(dll.tail.value)
+
+
+#'''''''''''''''Lecture Notes''''''''''''''''''
+    def delete_new(self, node):
+        # is there anything to delete?
+        if self.head is None and self.tail is None:
+            return
+        # check if there's only one node
+        if node is self.head and node is self.tail:
+            self.head = None
+            self.tail = None
+        # check if the node is the head
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        # check if the node is the tail
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        # otherwise, the node is some node in the middle
+        else:
+            node.delete()
+        # don't forget to decrement the length
+        self.length -= 1
+
+    def get_max_new(self):
+        # if the list is empty, return None
+        if self.head is None and self.tail is None:
+            return
+        # keep track of the largest value we've seen so far
+        max_value = self.head.value
+        # traverse the entirety of the linked list
+        current = self.head.next
+        
+        while current is not None:
+            # if we see a value > the largest value we've seen so far
+            if current.value > max_value:
+                # update the largest value
+                max_value = current.value
+            # update current to point do the next node
+            current = current.next
+        # return the largest value
+        return max_value
